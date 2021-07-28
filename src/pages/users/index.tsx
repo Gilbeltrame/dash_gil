884043b2daae3d18951a6ce70a434e95ced90e11
Gilberto Beltrame Neto <gilberto_abn@hotmail.com>
2021-07-27 23:17:38 -0300

    mirage fakeApi + reactQuery

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,11 +1,34 @@
-import { Box, Button, Text, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Text, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from "react-query"
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
+
 export default function UserList(){
+  const { data , isLoading, isFetching, error } = useQuery('users' , async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+    
+    const users = data.users.map(user => {
+      return{
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR',{
+          day: "2-digit",
+          month: "long",
+          year:"numeric"
+        })
+      }
+    })
+    return users;
+  }, {
+    staleTime: 1000 * 5
+  })
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -23,6 +46,7 @@ export default function UserList(){
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4"/> }
             </Heading>
             <Link href="/users/create" passHref>
               <Button
@@ -37,56 +61,72 @@ export default function UserList(){
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="32">
-                  <Checkbox colorScheme="pink"/>
-                </Th>
-                <Th>
-                  Usuário
-                </Th>
-                { isWideVersion && <Th> Data de cadastro </Th> }
-                <Th width="8">
+          {isLoading ? 
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          : error ? (
+            <Flex justify='center'>
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) :
+            (<>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="32">
+                      <Checkbox colorScheme="pink"/>
+                    </Th>
+                    <Th>
+                      Usuário
+                    </Th>
+                    { isWideVersion && <Th> Data de cadastro </Th> }
+                    <Th width="8">
 
-                </Th>
-              </Tr>
-            </Thead>
+                    </Th>
+                  </Tr>
+                </Thead>
 
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink"/>
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">
-                      Gilberto Neto
-                    </Text>
-                    <Text fontSize="sm" color="gray.300">
-                      gilberto.neto@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td> 04 de Abril, 2021 </Td>}
-                {isWideVersion && (
-                  <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                  >
-                    Editar
-                  </Button>
-                  </Td>
-                )}
-              </Tr>
-            </Tbody>
-          </Table>
+                <Tbody>
+                  {data.map((user) => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme="pink"/>
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">
+                              {user.name}
+                            </Text>
+                            <Text fontSize="sm" color="gray.300">
+                              {user.email}
+                            </Text>
+                          </Box>
+                        </Td>
+                        { isWideVersion && <Td> {user.createdAt} </Td>}
+                        {isWideVersion && (
+                          <Td>
+                          <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="purple"
+                            leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                          >
+                            Editar
+                          </Button>
+                          </Td>
+                        )}
+                      </Tr>
+                    )
+                  })}
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination />
+            </>)
+           }
         </Box>
       </Flex>
     </Box>
---
+++ src/services/mirage/index.ts
@@ -0,0 +1,51 @@
+import { createServer, Factory, Model } from 'miragejs'
+import faker from 'faker'
+
+type User = { //Criando a tipagem da fake data que o servidor vai criar
+  name: string;
+  email: string;
+  created_at: string;
+}
+
+export function makeServer(){
+  const server = createServer({
+
+    models:{
+      user: Model.extend<Partial<User>>({ //O partial serve para dizer que a info nem sempre vai ter todos os campos do tipo User
+
+      })
+    },
+
+    factories: { //factories fabricam dados em massa, criamos o tipo user com nome email e data de criação
+      user: Factory.extend({
+        name() {
+          return  faker.name.firstName()
+        },
+        email() {
+          const email = faker.internet.email()
+          return email.toLowerCase()
+        },
+        createdAt() {
+          return faker.date.recent(10)
+        },
+      })
+    },
+
+    seeds(server){
+      server.createList('user', 20) //geramos os dados da factory "user" no fake servidor, um total de 200 users
+    },
+
+    routes(){
+      this.namespace = 'api'; // nada mais que o localhost:3000/api pra fazer o fetch
+      this.timing = 750; // bom dar um timing em ms pra testar loaders, spinners etc na tela
+
+      this.get('/users');
+      this.post('/users');
+
+      this.namespace = ''; //por causa do next.js temos que usar
+      this.passthrough(); //fazer com que todas chamadas com a rota passem pelo mirage e se não forem detectadas, passem adiante sem erro.
+    }
+  })
+
+  return server;
+}
